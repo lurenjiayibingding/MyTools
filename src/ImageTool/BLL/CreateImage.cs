72f8f46739e8b9df5afc7d935e05ad8a5c5eb50f
lurenjia <ljiguang@163.com>
2022-05-04 20:21:11 +0800

    创建图片

---
+++ src/ImageTool/BLL/CreateImage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTool.BLL
+{
+    internal class CreateImage
+    {
+        /// <summary>
+        /// 保存图片
+        /// </summary>
+        public void SaveImage()
+        {
+            Bitmap map = new Bitmap(1080, 2340);
+            Graphics g = Graphics.FromImage(map);
+
+            Brush b = new SolidBrush(Color.FromArgb(0, 0, 0));
+            Rectangle rectangle = new Rectangle(0, 0, map.Width, map.Height);
+            g.FillRectangle(b, rectangle);
+
+            Font f = new Font("Cascadia Mono", 300, FontStyle.Bold);
+            //Brush b2 = System.Drawing.Brushes.White;
+            Brush b2 = new SolidBrush(Color.FromArgb(255, 255, 255));
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            RectangleF rect = new RectangleF(0, 350, map.Width, map.Height);
+            RectangleF rect2 = new RectangleF(0, 650, map.Width, map.Height);
+            RectangleF rect3 = new RectangleF(0, 950, map.Width, map.Height);
+            RectangleF rect4 = new RectangleF(0, 1250, map.Width, map.Height);
+            RectangleF rect5 = new RectangleF(0, 1550, map.Width, map.Height);
+
+            g.DrawString("圖", f, b2, rect, sf);
+            g.DrawString("樣", f, b2, rect2, sf);
+            g.DrawString("圖", f, b2, rect3, sf);
+            g.DrawString("森", f, b2, rect4, sf);
+            g.DrawString("破", f, b2, rect5, sf);
+            map.Save(@"D:\图片\图样.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
