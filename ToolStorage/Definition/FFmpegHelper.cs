425fcc784c47d564e8c4eb0b9498a5c8ed2bff03
lurenjia <ljiguang@163.com>
2024-02-22 23:22:29 +0800

    代码拆分

--- ToolStorage/Definition/FFmpegHelper.cs
+++ ToolStorage/Definition/FFmpegHelper.cs
@@ -31,89 +31,33 @@ namespace ToolStorage.Definition
         }
 
         /// <summary>
-        /// 得到一个目录中所有文件的文件名集合
-        /// </summary>
-        /// <param name="directoryPath">目录路径</param>
-        /// <returns>目录下的文件名集合</returns>
-        public string[] GetAllVideoName(string directoryPath)
-        {
-            if (string.IsNullOrWhiteSpace(directoryPath))
-            {
-                return new string[0];
-            }
-            if (!Directory.Exists(directoryPath))
-            {
-                return new string[0];
-            }
-            return Directory.GetFiles(directoryPath);
-        }
-
-        /// <summary>
         /// 将目录中的所有视频文件的文件名写入到一个文本文件中
         /// </summary>
         /// <param name="directoryPath">目录路径</param>
         /// <param name="listFileName">文本文件名</param>
         public void WriteListFile(string directoryPath, string listFileName = "video_list.txt")
         {
-            var fileNameList = GetAllVideoName(directoryPath).OrderBy(n => n.Length).ThenBy(n => n).ToArray();
+            var fileNameList = FileHelper.GetAllVideoName(directoryPath).OrderBy(n => n.Length).ThenBy(n => n).ToArray();
             StringBuilder strContent = new StringBuilder();
             int totalFileCount = fileNameList.Count();
             for (int i = 0; i < totalFileCount; i++)
             {
-                if (videoSuffix.Contains(GetSuffix(fileNameList[i]).ToUpperInvariant()))
+                if (videoSuffix.Contains(FileHelper.GetSuffix(fileNameList[i]).ToUpperInvariant()))
                 {
                     strContent.Append($"file '{fileNameList[i]}'{Environment.NewLine}");
                 }
                 if (i != 0 && i % bufferLine == 0)
                 {
-                    AppendText($@"{directoryPath}\{listFileName}", strContent.ToString());
+                    FileHelper.AppendText($@"{directoryPath}\{listFileName}", strContent.ToString());
                     strContent.Clear();
                 }
             }
             if (strContent.Length > 0)
             {
-                AppendText($@"{directoryPath}\{listFileName}", strContent.ToString());
+               FileHelper.AppendText($@"{directoryPath}\{listFileName}", strContent.ToString());
             }
         }
-
-        /// <summary>
-        /// 得到文件后缀
-        /// </summary>
-        /// <param name="filePath">文件路径</param>
-        /// <returns>文件的后缀</returns>
-        private string GetSuffix(string filePath)
-        {
-            return filePath.Split('.').Last();
-        }
-
-        /// <summary>
-        /// 向一个文本文件中追加内容
-        /// </summary>
-        /// <param name="textFilePath">文本文件的路径</param>
-        /// <param name="content">需要追加的内容</param>
-        private void AppendText(string textFilePath, string content)
-        {
-            FileMode mode;
-            if (!File.Exists(textFilePath))
-            {
-                mode = FileMode.Create;
-            }
-            else
-            {
-                mode = FileMode.Append;
-            }
-
-            var bytes = !string.IsNullOrWhiteSpace(content) ? Encoding.UTF8.GetBytes(content) : Array.Empty<byte>();
-
-            if (bytes != null && bytes.Length > 0)
-            {
-                using (FileStream fs = new FileStream(textFilePath, mode, FileAccess.Write))
-                {
-                    fs.Write(bytes, 0, bytes.Length);
-                }
-            }
-        }
-
+        
         public void Execute(string command)
         {
             using (Process p = new Process())
