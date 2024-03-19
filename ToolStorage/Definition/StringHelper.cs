7c030219ef80b043c4bea9450b18ecc590dbd7e6
lurenjia <ljiguang@163.com>
2024-03-19 20:37:19 +0800

    临时提交

---
+++ ToolStorage/Definition/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolStorage.Definition
+{
+    internal class StringHelper
+    {
+        public bool isEqual()
+        {
+            string.Compare("aaa","bbb",StringComparison.CurrentCulture)
+        }
+    }
+}
--- ToolStorage/Program.cs
+++ ToolStorage/Program.cs
@@ -10,7 +10,7 @@ namespace ToolStorage
             //ffmpeg.MergeAllVideo(@"C:\Users\刘继光的PC\Desktop\视频文件", @"C:\Users\刘继光的PC\Desktop\视频文件\new5.mp4");
 
             //ffmpeg.WriteListFile(@"D:\2024春节\2024021010");
-            ffmpeg.MergeAllVideo(@"D:\2024春节\2024021010", @"D:\2024春节\2024021010\merge.mp4");
+            ffmpeg.MergeAllVideo(@"E:\2024春节\20240205", @"E:\2024春节\20240205\20240205.mp4");
 
             Console.WriteLine("Hello, World!");
         }
