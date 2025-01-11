68be19fdf27ca9e0f9690da876eb38ae12de5bde
liujiguang <ljiguang@163.com>
2025-01-11 22:47:51 +0800

    新增创建jpg图片的方法

--- Go/ToolStorage/PictureHelper.go
+++ Go/ToolStorage/PictureHelper.go
@@ -2,6 +2,8 @@ package toolstorage
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"image/jpeg"
 	"image/png"
 	"os"
@@ -39,5 +41,39 @@ func SavePNGToJPG(inputPath string, outputPath string) error {
 	}
 
 	fmt.Print("图片" + inputPath + "转为" + outputPath + "成功")
+	return nil
+}
+
+// 创建一个指定宽度、高度和rgb颜色值的jgp图片。
+// width:图片宽度，height:图片高度，r:红色值，g:绿色值，b:蓝色值，savePath:保存路径
+func CreateJpgPricture(width, height int, r, g, b uint8, savePath string) error {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	//A:255表示完全不透明
+	//go中实例化一个结构体时，要么带着全部的参数名，要么全部不带，不能只带一部分
+	colorValue := color.RGBA{r, g, b, 255}
+	// colorValue := color.RGBA{R: r, G: g, B: b, A: 255}
+
+	//将颜色填充到整个图片中
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			img.Set(x, y, colorValue)
+		}
+	}
+
+	file, err := os.Create(savePath)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	defer file.Close()
+
+	options := jpeg.Options{Quality: 100}
+
+	err1 := jpeg.Encode(file, img, &options)
+	if err1 != nil {
+		fmt.Print(err1)
+		return err1
+	}
+	print("图片" + savePath + "创建成功")
 	return nil
 }
--- Go/main.go
+++ Go/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	err := toolstorage.SavePNGToJPG("D:\\图片\\background.png", "D:\\图片\\background.jpg")
+	// err := toolstorage.SavePNGToJPG("D:\\图片\\background.png", "D:\\图片\\background.jpg")
+	err := toolstorage.CreateJpgPricture(1920, 1080, 252, 252, 252, "D:\\图片\\background.jpg")
 	if err != nil {
 		print(err)
 	}
